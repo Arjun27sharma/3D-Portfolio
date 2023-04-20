0df32f738b6ed6d16debcb19188e8941ecb7a318
Arjun Sharma <arjunsharma6622@gmail.com>
2023-04-21 00:03:15 +0530

    hoc added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { Tilt } from 'react-tilt'
 import { styles } from '../style'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from "../hoc"
 
 
 const ServiceCard = ({index, title, icon}) => {
@@ -21,6 +22,8 @@ const ServiceCard = ({index, title, icon}) => {
           }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
+          <img src={icon} alt={title} className='h-16 w-16 object-contain'/>
+          <h3 className='text-white text-[20px] shadow-card'>{title}</h3>
 
         </div>
       </motion.div>
